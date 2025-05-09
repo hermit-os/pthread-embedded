27ffa014913e95cdf1e77ebfe5b7b303e38a9d9e
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-05-09 18:17:57 +0200

    fix(pthread.h): move types from pthread.h to _pthreadtypes.h
    This makes the types appear when including sys/types.h, as required by POSIX.

--- pthread.h
+++ pthread.h
@@ -385,31 +385,6 @@ enum
 #include <sched.h>
 
     /*
-     * Generic handle type - intended to extend uniqueness beyond
-     * that available with a simple pointer. It should scale for either
-     * IA-32 or IA-64.
-     */
-    typedef struct
-      {
-        void * p;                   /* Pointer to actual object */
-        unsigned int x;             /* Extra information - reuse count etc */
-      } pte_handle_t;
-
-    typedef pte_handle_t pthread_t;
-    typedef struct pthread_attr_t_ * pthread_attr_t;
-    typedef struct pthread_once_t_ pthread_once_t;
-    typedef struct pthread_key_t_ * pthread_key_t;
-    typedef struct pthread_mutex_t_ * pthread_mutex_t;
-    typedef struct pthread_mutexattr_t_ * pthread_mutexattr_t;
-    typedef struct pthread_cond_t_ * pthread_cond_t;
-    typedef struct pthread_condattr_t_ * pthread_condattr_t;
-    typedef struct pthread_rwlock_t_ * pthread_rwlock_t;
-    typedef struct pthread_rwlockattr_t_ * pthread_rwlockattr_t;
-    typedef struct pthread_spinlock_t_ * pthread_spinlock_t;
-    typedef struct pthread_barrier_t_ * pthread_barrier_t;
-    typedef struct pthread_barrierattr_t_ * pthread_barrierattr_t;
-
-    /*
      * ====================
      * ====================
      * POSIX Threads
